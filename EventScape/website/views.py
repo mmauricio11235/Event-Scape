c79e0bb38ec4c826708dd5e8d5c6c94c63ab108d
Dima Smirnov <dimkasmir@gmail.com>
2014-12-03 19:25:59 -0800

    Fixes signals

--- EventScape/website/views.py
+++ EventScape/website/views.py
@@ -1,9 +1,5 @@
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
-from django.shortcuts import render
 
 class RegisterUser(CreateView):
     model = User
-
-def index(request):
-	 return render(request, 'website/index.html')
