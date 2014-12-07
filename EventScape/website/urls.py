4d3bd3b89b479df6741c454913fd80bf8b33cc6a
Dima Smirnov <dimkasmir@gmail.com>
2014-12-06 18:06:22 -0800

    Bring back profile URL

--- EventScape/website/urls.py
+++ EventScape/website/urls.py
@@ -2,27 +2,33 @@ from django.conf.urls import patterns, url
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.models import User
 
 from .views import RegisterUser, EventAdd, EventEdit, EventSearch
 from .models import Event
 
-###FOR TESTING ONLY###
+# ##FOR TESTING ONLY###
 from django.conf import settings
 from django.conf.urls.static import static
-######################
+# #####################
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.contrib.auth.views.login', name="login"),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),
-    url(r'^register/$', RegisterUser.as_view(), name="register"),
-    url(r'^event/new$', EventAdd.as_view(), name="event-new"),
-    url(r'^event/delete/(?P<pk>\d+)/$', DeleteView.as_view(model=Event,
-                                                           template_name='event/delete_confirm.html',
-                                                           success_url=reverse_lazy('event-search')), name="event-delete"),
-    url(r'^event/edit/(?P<pk>\d+)/$', EventEdit.as_view(), name="event-edit"),
-    url(r'^event/(?P<pk>\d+)/$', DetailView.as_view(model=Event,
-                                                    template_name='event/detail.html'), name="event-detail"),
-    url(r'^event/search$', EventSearch.as_view(), name="event-search"),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #FOR TESTING ONLY
+                       url(r'^$', 'django.contrib.auth.views.login', name="login"),
+                       url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),
+                       url(r'^register/$', RegisterUser.as_view(), name="register"),
+                       url(r'^event/new$', EventAdd.as_view(), name="event-new"),
+                       url(r'^event/delete/(?P<pk>\d+)/$', DeleteView.as_view(model=Event,
+                                                                              template_name='event/delete_confirm.html',
+                                                                              success_url=reverse_lazy('event-search')),
+                           name="event-delete"),
+                       url(r'^event/edit/(?P<pk>\d+)/$', EventEdit.as_view(), name="event-edit"),
+                       url(r'^event/(?P<pk>\d+)/$', DetailView.as_view(model=Event,
+                                                                       template_name='event/detail.html'),
+                           name="event-detail"),
+                       url(r'^event/search$', EventSearch.as_view(), name="event-search"),
+                       url(r'^user/profile/(?P<pk>\d+)/$', DetailView.as_view(model=User,
+                                                                              template_name='website/profile.html'),
+                           name="user-profile"),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  #FOR TESTING ONLY
 
