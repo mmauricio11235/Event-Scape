c1919f02551ed0e34e618fcf00666ace7e9160af
Dima Smirnov <dimkasmir@gmail.com>
2014-12-08 21:28:19 -0800

    Add EventImage admin

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Event, Tag
+from .models import Event, Tag, EventImage
 
 admin.site.register(Tag)
 admin.site.register(Event)
+admin.site.register(EventImage)
