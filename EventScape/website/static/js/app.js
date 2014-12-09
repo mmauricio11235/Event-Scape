9d9372c641b3e0f2f78b3745818737c12d898b9c
Mauricio Molina <mmauricio11235@gmail.com>
2014-12-09 04:42:30 +0000

    several fixes. Added subscribe to users

--- EventScape/website/static/js/app.js
+++ EventScape/website/static/js/app.js
@@ -3,8 +3,22 @@ $(document).ready(function(){
     var obj;
     obj = $(this).attr("data-event");
      $.get("/event/attend", {eid: obj}, function(data){
+               if (data==='true' && $(".titleOfEvent").children().size() < 2){
+               		$(".titleOfEvent").append("<h2 style = 'text-align: center;color: blue;'> Attending! </h2>")
+               }
+
+           });
+});
+
+$('.follow').click(function(){
+    var obj;
+    obj = $(this).attr("user-id");
+     $.get("/user/follow", {uid: obj}, function(data){
                if (data==='true'){
-               		$(".jumbotron").append("<h1> ATTENDING </h1>")
+               		window.alert("Working");
+               }
+               else{
+               	window.alert(data);	
                }
 
            });
